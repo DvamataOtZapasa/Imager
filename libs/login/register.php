b59d30719c86d910beeb6ec41ba9e08bd983919b
VICTOR <dvamataotzapasa@abv.bg>
2017-03-03 15:06:19 +0200

    1

--- libs/login/register.php
+++ libs/login/register.php
@@ -5,4 +5,15 @@ $username = $_POST['username'];
 $pass1 = $_POST['pass1'];
 $pass2 = $_POST['pass2'];
 
+$_SESSION['ERROR'] = "";
+
 $result = mysqli_query($con,"SELECT none FROM users WHERE username=$username");
+if(mysqli_num_rows($result) == 0){
+    if($pass1 == $pass2){
+        mysqli_query($con,"INSTER in");
+    }else{
+        $_SESSION['ERROR'] = "Passwords do not match;";
+    }
+}else{
+    $_SESSION['ERROR'] = "Username already taken";
+}
