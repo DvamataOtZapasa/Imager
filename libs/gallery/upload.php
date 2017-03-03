56413e908e2c690b261f4a083487056d0b4d0c29
VICTOR <dvamataotzapasa@abv.bg>
2017-03-03 15:31:24 +0200

    1

--- libs/gallery/upload.php
+++ libs/gallery/upload.php
@@ -6,8 +6,8 @@ if(isset($_FILES['fileToUpload']['name'])){
     if(strpos($_FILES['fileToUpload']['type'], "image") !== false && $_FILES['fileToUpload']['size'] <= 20000000){
 
 
-        $author = $_SESSION['user'];
-        $name = $_POST['name']
+        $author = $_SESSION['id'];
+        $name = $_POST['name'];
 
         $imagetype = $_FILES['fileToUpload']['type'];
 
@@ -15,18 +15,14 @@ if(isset($_FILES['fileToUpload']['name'])){
 
             $imagetype = str_replace("image/", "", $imagetype);
             require '../dbConfig.php';
-            mysqli_query($con, "INSERT INTO `images`(`name`, `author`) VALUES ('$author','$name',)");
+            mysqli_query($con, "INSERT INTO `images`(`name`, `author`) VALUES ('$name','$author')");
             $id = mysqli_query($con, "SELECT `AUTO_INCREMENT` FROM  INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '$dbName' AND   TABLE_NAME   = 'gallery'");
-            echo 'INSERT INTO `gallery`(`name`, `author`, `desc`, `rating`, `category`) VALUES ("'.$title.'","'.$author.'","'.$desc.'",0,'.$cat.')';
             $id = mysqli_fetch_assoc($id);
             $id = $id['AUTO_INCREMENT']-1;
             echo "../../images/$id".".".$imagetype;
-            move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "../../images/$id".".".$imagetype);
-            Resize_Image("$id.$imagetype", $id, 480, 480, "../../images/", "../../images/thumbnails/",$imagetype);
-            rename("../../images/$id.$imagetype", "../../images/$id");
-            $message = 'Congratulations!  Your file was accepted.';
+            move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "../../images/$id");
         }
     }
 }
-header("Location:/site/gallery")
+//header("Location:/site/gallery")
 ?>
