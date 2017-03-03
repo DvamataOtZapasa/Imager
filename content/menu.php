e0cb8aff459e929506aebbcd553018dd88cf787b
VICTOR <dvamataotzapasa@abv.bg>
2017-03-03 15:28:35 +0200

    1

--- content/menu.php
+++ content/menu.php
@@ -3,8 +3,8 @@
     <ul id="menuList">
         <li><a href="index.php">Home</a></li>
         <li><a href="gallery.php">Gallery</a></li>
-        <li><a>Contacts</a></li>
-        <li onclick="showLogin()"><a href="login.php">Login</a></li>
+        <li><a href="contacs.php">Contacts</a></li>
+        <li onclick="showLogin()">Login</li>
     </ul>
 
 </div>
