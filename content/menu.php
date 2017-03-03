24b173dc9e8187bf2409ebe87379d90e5631b0a3
VICTOR <dvamataotzapasa@abv.bg>
2017-03-03 16:15:14 +0200

    1

--- content/menu.php
+++ content/menu.php
@@ -1,9 +1,9 @@
 <div id="menuContainer">
 
     <ul id="menuList">
-        <li><a href="index.php">Home</a></li>
-        <li><a href="gallery/">Gallery</a></li>
-        <li><a href="contacs.php">Contacts</a></li>
+        <li><a href="/index.php">Home</a></li>
+        <li><a href="/gallery/">Gallery</a></li>
+        <li><a href="/contacs.php">Contacts</a></li>
         <li onclick="showLogin()">Login</li>
     </ul>
     <form action="libs/login/login.php" method="post" id="loginForm">
