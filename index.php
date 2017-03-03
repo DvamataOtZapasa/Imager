4d71ad1b0f2a61f2fdce5cc02772cf77665d8762
VICTOR <dvamataotzapasa@abv.bg>
2017-03-03 16:47:11 +0200

    1

--- index.php
+++ index.php
@@ -13,35 +13,7 @@ require_once "libs/dbConfig.php"
             require_once "content/menu.php"
         ?>
         <div id="container">
-
-            <?php
-                if(isset($_SESSION['user'])){
-                    ?>
-                    Lognat si <?= $_SESSION['user']; ?>
-
-                    <form action="libs/login/logout.php" method="post">
-                        <input type="submit" value="logout">
-                    </form>
-
-
-                    <?php
-                }else{
-                    ?>
-
-                    <form action="libs/login/register.php" method="post">
-                        Username:
-                        <input name="username" type="text">
-                        Password:
-                        <input name="pass1" type="password">
-                        Password again:
-                        <input name="pass2" type="password">
-                        <input type="submit">
-                    </form>
-
-                    <?php
-                }
-            ?>
-
+            <p>Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content Content </p>
         </div>
         <script src="/js/js.js"></script>
     </body>
