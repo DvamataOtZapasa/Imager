8bebb15c3aca734686300b4b52f825982dd8febf
VICTOR <dvamataotzapasa@abv.bg>
2017-03-03 14:14:00 +0200

    1

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
-<?php
-?>
+
 <html>
     <title>Imager</title>
+    <link type="text/css" rel="stylesheet" href="css/main.css" >
     <body>
-        <div id="menu"></div>
+        <div id="menuContainer">
+
+            <ul id="menuList">
+                <li>Home</li>
+                <li>Gallery</li>
+                <li>Contacts</li>
+            </ul>
+
+        </div>
         <div id="container">
-            
+
         </div>
     </body>
 </html>
