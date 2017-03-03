a737be366e4dc2a9c6298fb9abfa1c1c1ba15cf2
VICTOR <dvamataotzapasa@abv.bg>
2017-03-03 14:27:21 +0200

    1

---
+++ libs/dbConfig.php
@@ -0,0 +1,14 @@
+<?php 
+$dbHost = "localhost";
+$dbName = "imager";
+
+$logT = "users";
+
+$dbUsername = "root";
+$dbPass = "123456";
+
+$con = mysqli_connect($dbHost,$dbUsername,$dbPass) or die ("could not connect to mysql");
+mysqli_select_db($con,$dbName) or die ("no database");   
+mysqli_query($con,"SET NAMES 'utf8'; CHARSET 'utf8';");
+
+?>
